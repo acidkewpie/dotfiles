44c35ac8706f4413d5c05c49ba21cc4e5f8aa009
acidkewpie <chris@untrepid.com>
2023-03-25 09:10:03 +0000

    Sat 25 Mar 09:10:03 GMT 2023

--- dot_config/i3/executable_load_startup_apps.sh
+++ dot_config/i3/executable_load_startup_apps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+xsetwacom --set "Wacom HID 52CD Finger touch" MaptoOutput eDP-1
 gnome-keyring-daemon --start --components=ssh,secrets,pkcs11
 i3-msg workspace 2:
 sleep 2
--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-# Terminate already running bar instances
-# killall -q polybar
-
-# Wait until the processes have been shut down
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar1 and bar2
-#MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
-
-#MONITORS=$MONITORS polybar default &
-#MONITOR=$MONITORS polybar default;
-
-#polybar default
-
-touch ~/.config/polybar/config.ini
-for m in $(polybar -m | cut -d: -f1); do
-  pgrep -f "polybar --reload $m -l error" || polybar --reload $m -l error&
-done
+MONS=$(polybar -m | wc -l)
+BARS=$(pgrep -c polybar)
+if [ "$MONS" -eq "$BARS" ]
+then
+  touch ~/.config/polybar/config.ini
+elif [ "$MONS" -eq "1" ]
+then
+  pkill polybar
+  polybar --reload default &
+else
+  pkill polybar
+  for m in $(polybar -m | cut -d: -f1); do
+    pgrep -f "polybar --reload $m" || polybar --reload $m &
+  done
+fi
 
 echo "Bars launched..."
 
