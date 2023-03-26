a1aba29005aa29286cff38dd5238aa7838ac4e6e
acidkewpie <chris@untrepid.com>
2023-03-26 19:51:05 +0100

    Sun 26 Mar 19:51:05 BST 2023

--- dot_config/i3/executable_load_startup_apps.sh
+++ dot_config/i3/executable_load_startup_apps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+onboard &
 xsetwacom --set "Wacom HID 52CD Finger touch" MaptoOutput eDP-1
 gnome-keyring-daemon --start --components=ssh,secrets,pkcs11
 i3-msg workspace 2:
-sleep 2
+sleep 1
 alacritty &
 alacritty &
-sleep 10
+sleep 5
 i3-msg workspace 1:
 google-chrome --password-store=gnome &
 
--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
+#!/bin/sh -x
 
-MONS=$(polybar -m | wc -l)
+MONS=($(polybar -m | cut -d: -f1))
 BARS=$(pgrep -c polybar)
-if [ "$MONS" -eq "$BARS" ]
+if [ "${#MONS[@]}" -eq "$BARS" ]
 then
   touch ~/.config/polybar/config.ini
-elif [ "$MONS" -eq "1" ]
-then
-  pkill polybar
-  polybar --reload default &
 else
   pkill polybar
-  for m in $(polybar -m | cut -d: -f1); do
-    pgrep -f "polybar --reload $m" || polybar --reload $m &
-  done
+  pidwait polybar
+  if [ "${#MONS[@]}" -eq "1" ]
+  then
+    MONITOR=$MONS polybar --reload default &
+  else
+    for m in ${MONS[@]}; do
+      polybar --reload $m &
+    done
+  fi
 fi
 
 echo "Bars launched..."
