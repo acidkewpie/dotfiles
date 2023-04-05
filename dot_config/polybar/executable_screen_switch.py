98620ce286efc6deb0abb4b117919901cb4d2316
acidkewpie <chris@untrepid.com>
2023-04-05 16:28:14 +0100

    Add .config/polybar/.config.ini.swp Update .config/polybar/screen_switch.py

--- dot_config/polybar/executable_screen_switch.py
+++ dot_config/polybar/executable_screen_switch.py
@@ -6,13 +6,13 @@ import evdev
 device_name, _s = 'SayoDevice SayoDevice 2x3F', 0
 
 RED = '%{F' + (environ.get('RED') or '#F00') + '}'
-GREEN = '%{F' + (environ.get('GREEN') or '#0F0') + '}'
-BLUE = '%{F' + (environ.get('BLUE') or '#00F') + '}'
+YELLOW = '%{F' + (environ.get('YELLOW') or '#0F0') + '}'
+HIGHLIGHT = '%{F' + (environ.get('HIGHLIGHT') or '#00F') + '}'
 
 def set_source(s):
     global _s 
     source = {'DP': '15', 'HDMI': '17'}
-    print(BLUE if s == 'HDMI' else GREEN, "", sep='', flush=True)
+    print(YELLOW if s == 'HDMI' else HIGHLIGHT, "", sep='', flush=True)
     if _s != s:
         run(["/usr/bin/ddccontrol", "-r", "0x60", "-w", source[s], 
             "dev:/dev/i2c-16"], capture_output=True, timeout=10)
