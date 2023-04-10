bdd9bd5a773703a8416327011cc848d579495145
acidkewpie <chris@untrepid.com>
2023-04-10 09:20:44 +0100

    Update .config/i3/load_startup_apps.sh

--- dot_config/i3/executable_load_startup_apps.sh
+++ dot_config/i3/executable_load_startup_apps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 gnome-keyring-daemon --start --components=ssh,secrets,pkcs11
-i3-msg workspace 2:
+i3-msg workspace 2
 sleep 1
 alacritty &
 alacritty &
 sleep 5
-i3-msg workspace 1:
+i3-msg workspace 1
 google-chrome --password-store=gnome &
 
