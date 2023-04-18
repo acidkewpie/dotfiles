e3ce280960191c4ab3ca9b5ae06bb02a117e28fc
acidkewpie <chris@untrepid.com>
2023-04-18 10:34:18 +0100

    Update .config/autorandr/desktop/config Update .config/autorandr/desktop/setup Update .config/picom/picom.conf Update .config/polybar/config.ini Update .config/polybar/mouse_on_the_keys.py

--- dot_config/polybar/executable_mouse_on_the_keys.py
+++ dot_config/polybar/executable_mouse_on_the_keys.py
@@ -5,39 +5,49 @@ from functools import partial
 from os import environ
 
 RED = '%{F' + (environ.get('RED') or '#F00') + '}'
-GREEN = '%{F' + (environ.get('GREEN') or '#0F0') + '}'
-BLUE = '%{F' + (environ.get('BLUE') or '#00F') + '}'
+YELLOW = '%{F' + (environ.get('YELLOW') or '#0F0') + '}'
+ORANGE = '%{F' + (environ.get('ORANGE') or '#FF0') + '}'
+HIGHLIGHT = '%{F' + (environ.get('HIGHLIGHT') or '#00F') + '}'
 
 context = Context()
 monitor = Monitor.from_netlink(context)
 monitor.filter_by('hid')
 
-for device in context.list_devices(subsystem='hid'):
-    if 'K850' in device.get('HID_NAME'):
-        keyboard = True
-    if 'M720' in device.get('HID_NAME'):
-        mouse = True
+for line in run(["/usr/bin/ddcutil", "detect", "--sleep-multiplier", ".5"], capture_output=True).stdout.decode('utf8').split('\n'):
+    if 'I2C bus' in line: i2c = line.split('-')[1].strip()
+    if '1179911201181' in line: break
+print(i2c)
 
+def set_source(s):
+    if s not in run(["/usr/bin/ddcutil", "getvcp", "0x60", "--sleep-multiplier", ".2", "--bus", i2c], capture_output=True).stdout.decode('utf8'):
+        run(["/usr/bin/ddcutil", "setvcp", "0x60", {'DisplayPort':'15','HDMI':'17'}[s], "--sleep-multiplier", ".2", "--bus", i2c], 
+            capture_output=True, timeout=10)
+    return(YELLOW if s == 'HDMI' else HIGHLIGHT)
+
+def get_hids():
+    devices=[d.get('HID_NAME') for d in context.list_devices(subsystem='hid')]
+    k = True if 'Keyboard K850' in devices else False
+    m = True if 'M720 Triathlon' in devices else False
+    u = True if 'Logitech USB Receiver' in devices else False
+    return (m, k, u)
+
+source=ORANGE
 while True:
-
+    (mouse, keyboard, usb) = get_hids()
     timeout = 300
-    if mouse is not keyboard:
+    if usb:
+        print(ORANGE + '',end='')
+    elif mouse is not keyboard:
         timeout = 5
         run(['/usr/bin/aplay', '/etc/sounds/honk.wav', '-q'])
-        print(GREEN + '' + RED + '' if mouse else RED + '' + GREEN + '', flush=True)
+        print(HIGHLIGHT + '' + RED + '' if mouse else RED + '' + HIGHLIGHT + '',end='')
     elif mouse and keyboard:
-        print(GREEN + '', flush=True)
+        source=set_source('DisplayPort')
+        print(HIGHLIGHT + '',end='')
     else: 
-        print(BLUE + '', flush=True)
-
+        source=set_source('HDMI')
+        print(YELLOW + '',end='')
+    print(source +'', flush=True)
     for device in iter(partial(monitor.poll, timeout), None):
-        if 'K850' in device.get('HID_NAME') and device.action == 'add':
-            keyboard = True
-        if 'K850' in device.get('HID_NAME') and device.action == 'remove':
-            keyboard = False
-        if 'M720' in device.get('HID_NAME') and device.action == 'add':
-            mouse = True
-        if 'M720' in device.get('HID_NAME') and device.action == 'remove':
-            mouse = False
         break
 
